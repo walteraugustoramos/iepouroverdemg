01e88d449d783bc6e763313e03db1bfcd8b75f63
Augusto Ramos <walteraugusto10@hotmail.com>
2016-08-21 12:10:14 -0300

    Organização das imagens dos grupos de oração

--- index.php
+++ index.php
@@ -89,15 +89,15 @@
       <div class="row">
         <div class="col-md-10 col-md-offset-1">
           <div class="col-md-4">
-            <img class="img-responsive thumbnail" src="img/grupo-adoradores-do-rei.jpg" alt="">
+            <img class="img-responsive thumbnail" src="img/grupos/grupo-adoradores-do-rei.jpg" alt="">
           </div>
 
           <div class="col-md-4">
-            <img class="img-responsive thumbnail" src="img/grupo-adoradores-por-excelencia.jpg" alt="">
+            <img class="img-responsive thumbnail" src="img/grupos/grupo-adoradores-por-excelencia.jpg" alt="">
           </div>
 
           <div class="col-md-4">
-            <img class="img-responsive thumbnail" src="img/grupo-lirio-dos-vales.jpg" alt="">
+            <img class="img-responsive thumbnail" src="img/grupos/grupo-lirio-dos-vales.jpg" alt="">
           </div>
         </div>
       </div>
