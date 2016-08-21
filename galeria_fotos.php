6f2999c7330ff3a548e326f8c337dc8021ce79ab
Augusto Ramos <walteraugusto10@hotmail.com>
2016-08-21 20:41:52 -0300

    Inclusão do album de fotos do 2° Culto de Missoes na galeria de fotos

--- galeria_fotos.php
+++ galeria_fotos.php
@@ -3,6 +3,23 @@
 	include 'includes/header.php';
 ?>
 
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4">
+			<center><h4>Galeria de Fotos da IEP Ouro Verde de Minas</h4></center>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="col-md-3">
+			<a href="segundo_culto_missoes.php">
+				<img src="img/galeria_fotos/segundo_culto_missoes/img-capa.jpg" alt="" class="img-responsive img-thumbnail">
+			</a>
+			<center><p>2° Culto de Missões</p></center>
+		</div>
+	</div>
+</div>
+
 <!--Inclusão do Footer-->
 <?php
 	include 'includes/footer.php';
