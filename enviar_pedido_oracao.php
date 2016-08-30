521196ec86bad2cb82bf3b557471eef63c46ddb1
Augusto Ramos <walteraugusto10@hotmail.com>
2016-08-29 21:58:49 -0300

    Inclusão da biblioteca PHPMailer para o envio de pedidos de oração e conclusão do envio de pedidos de oração

---
+++ enviar_pedido_oracao.php
@@ -0,0 +1,76 @@
+<?php
+	session_start();
+	// Verifica se a variável $_POST não é vazia...
+	// ou seja: houve um submit no formulário de pedido de oração
+	if(!empty($_POST)){
+
+		foreach($_POST as $key => $value){
+			$$key = $value;
+		}
+
+		// Caminho da biblioteca PHPMailer
+		require 'PHPMailer/PHPMailerAutoload.php';
+
+		// Instância do objeto PHPMailer
+		$mail = new PHPMailer;
+
+		// Seta a linguagem das mensagens de erro
+		$mail->setLanguage = 'pt';
+
+		// Configura para envio de e-mails usando SMTP
+		$mail->isSMTP();
+
+		// Servidor SMTP
+		$mail->Host = 'smtp.gmail.com';
+
+		// Usar autenticação SMTP
+		$mail->SMTPAuth = true;
+
+		// Usuário da conta
+		$mail->Username = 'iep.ouroverdemg@gmail.com';
+
+		// Senha da conta
+		$mail->Password = 'paulohenrique12345';
+
+		// Tipo de encriptação que será usado na conexão SMTP
+		$mail->SMTPSecure = 'ssl';
+
+		// Porta do servidor SMTP
+		$mail->Port = 465;
+
+		// Informa se vamos enviar mensagens usando HTML
+		$mail->IsHTML(true);
+
+		// Email do Remetente
+		$mail->From = $email;
+
+		// Nome do Remetente
+		$mail->FromName = $nome;
+
+		// Email e Nome para resposta
+		$mail->addReplyTo($email,$nome);
+
+		// Endereço do e-mail do destinatário
+		$mail->addAddress('iep.ouroverdemg@gmail.com');
+
+		// Assunto do e-mail
+		$mail->Subject = utf8_decode($nome.' lhe enviou um pedido de oração');
+
+		// Mensagem que vai no corpo do e-mail
+		$mail->Body = "<p>Nome: $nome</p>
+		<p><b>Email</b>: $email</p>
+		<p><b>Estado</b>: $estado</p>
+		<p><b>Cidade</b>: $cidade</p>
+		<p><b>Pedido Oração</b>: $pedido</p>
+		";
+
+		// Envia o e-mail e captura o sucesso ou erro
+		if($mail->Send()):
+		    $_SESSION['msg']['success'] = 'Pedido de Oração Enviado com Sucesso';
+			header('Location:pedido_oracao.php');
+		else:
+		    $_SESSION['msg']['error'] = 'Erro ao enviar Pedido de Oração:' . $mail->ErrorInfo;
+			header('Location:pedido_oracao.php');
+		endif;
+	}
+?>
--- pedido_oracao.php
+++ pedido_oracao.php
@@ -1,3 +1,7 @@
+<!--Start Session Msg-->
+<?php
+  session_start();
+?>
 <!--Inclusão do header-->
 <?php
   include 'includes/header.php';
@@ -8,13 +12,35 @@
         <img src="img/pedido-de-oracao.jpg" class="img-responsive" alt="" height="200" width="100%">
       </div>
     </div>
+    
+    <div class="row" style="margin-top:2%;">
+      <div class="col-md-4 col-md-offset-4">
+        <?php
+          if(!empty($_SESSION['msg']['success'])){
+        ?>
+        <div class="alert alert-success" role="alert">
+          <center><?=$_SESSION['msg']['success']?></center>
+        </div>
+        <?php
+          unset($_SESSION['msg']['success']);
+          }else if(!empty($_SESSION['msg']['error'])){
+        ?>
+          <div class="alert alert-danger" role="alert">
+            <center><?=$_SESSION['msg']['error']?></center>
+          </div>
+        <?php
+          unset($_SESSION['msg']['error']);
+          }//fechamento do else if
+        ?>
+      </div><!--col-md-4 col-md-offset-4-->
+    </div><!--row-msg-->
 
     <div class="row" style="margin-top:5%;">
       <div class="col-md-10 col-md-offset-1">
         <fieldset>
           <legend><center>Faça Seu Pedido De Oração</center></legend>
 
-          <form action="">
+          <form action="enviar_pedido_oracao.php" method="post">
             <div class="row">
               <div class="col-md-4 col-md-offset-2">
                 <label for="nome">Nome: </label>
@@ -23,7 +49,7 @@
 
               <div class="col-md-4">
                 <label for="email">Email: </label>
-                <input type="email" name="email" class="form-control" placeholder="Email">
+                <input type="email" name="email" class="form-control" placeholder="Email" required>
               </div>
             </div>
 
@@ -41,8 +67,8 @@
 
             <div class="row">
               <div class="col-md-4 col-md-offset-2">
-                <label for="pedido-oracao">Pedido: </label>
-                <textarea name="pedido-oracao" class="form-control" id="pedido-oracao" cols="30" rows="10"></textarea>
+                <label for="pedido">Pedido: </label>
+                <textarea name="pedido" class="form-control" id="pedido" cols="30" rows="10" required></textarea>
               </div>
             </div>
 
