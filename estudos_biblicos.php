415ea00c3bcba908bdfd4ba26ce6194e2122ea16
Augusto Ramos <walteraugusto10@hotmail.com>
2016-09-03 14:15:57 -0300

    Inclusão dos estudos biblicos na pagina de estudos biblicos

--- estudos_biblicos.php
+++ estudos_biblicos.php
@@ -12,14 +12,14 @@
 
 	<div class="row">
 		<div class="col-md-3">
-			<a href="#">
+			<a href="estudos_biblicos/Aguia-e-o-Cristao.pdf" target="_blank">
 				<img src="img/estudos_biblicos/aguia-e-o-cristao.jpg" alt="" class="img-responsive img-thumbnail">
 			</a>
 			<center><p>Águia e o Cristão</p></center>
 		</div>
 
 		<div class="col-md-3">
-			<a href="#">
+			<a href="estudos_biblicos/Plano-de-Salvacao.pdf" target="_blank">
 				<img src="img/estudos_biblicos/plano-de-salvacao.jpg" alt="" class="img-responsive img-thumbnail">
 			</a>
 			<center><p>Plano de Salvação</p></center>
